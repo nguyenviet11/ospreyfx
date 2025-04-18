4b181822a63b3f6210508fa34b7753dcf38afc79
vietnv <nguyenvanvietbn1998@gmail.com>
2025-04-18 22:24:55 +0700

    up

--- src/app/ospreyfx-connecting/page.js
+++ src/app/ospreyfx-connecting/page.js
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 export default function page() {
     useEffect(() => {
       const timeout = setTimeout(() => {
-        window.location.href = "https://hubbrokers.vn/top-forext-ospreyfx";
+        window.location.href = "https://gameplant.us/";
       }, 600);
       return () => clearTimeout(timeout);
       }, []);
--- src/app/page.js
+++ src/app/page.js
@@ -6,39 +6,14 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [status, setStatus] = useState(true);
-  const [hasRedirected, setHasRedirected] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const handleMouseMove = () => {
-    if (!hasRedirected) {
-      setHasRedirected(true);
-      window.location.href = "/ospreyfx-connecting"; 
-    }
-  };
+
   const handleClick = () => {
       window.location.href = "/ospreyfx-connecting"; 
   }
-  const fetchItem = async () => {
-    try {
-        const response = await fetch(`https://db-affiliate.onrender.com/status?id=1`);
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setStatus(data?.status);
-    } catch (error) {
-        console.error("Error fetching item:", error);
-    } finally {
-      setIsLoading(false);
-    }
-};
-  useEffect(() => {
-    fetchItem()
-  },[])
+  
   return (
     <div
-      onMouseMove={!isLoading && status ? handleMouseMove : null}
-      onClick={!isLoading && !status ? handleClick : null}
+      onClick={handleClick}
     >
       <main className={styles.main}>
         <h3 className={styles.text}>
